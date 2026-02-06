c298b93bc95d8706dbdb04c667d56d46079b4b07
Doug Reynolds <douglas.j.reynolds@gmail.com>
2026-02-06 11:33:57 -0500

    Updated nvim-treesitter and moved to lua file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,11 +1,5 @@
+vim.cmd([[
 autocmd BufNewFile,BufReadPost * if &filetype == "yaml" | set expandtab shiftwidth=2 indentkeys-=0# | endif
-
-silent! autocmd! filetypedetect BufRead,BufNewFile *.tf
-autocmd BufRead,BufNewFile *.hcl set filetype=hcl
-autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl
-autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform
-autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json
-autocmd BufWritePre *.py call BlackSync()
 
 
 "set termguicolors
@@ -14,16 +8,26 @@ autocmd BufWritePre *.py call BlackSync()
 if !has('gui_running')
   set t_Co=256
 endif
-
-lua << EOF
+]])
 
 local home = os.getenv("HOME")
 
 vim.g.python3_host_prog = home .. "/.local/venv/nvim/bin/python"
 
-vim.g.terraform_fmt_on_save=1
-vim.g.terraform_align=1
-vim.g.terraform_binary_path='/usr/local/bin/tofu'
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+    callback = function(args)
+        -- 2
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            -- 3
+            buffer = args.buf,
+            callback = function()
+                -- 4 + 5
+                vim.lsp.buf.format { async = false, id = args.data.client_id }
+            end,
+        })
+    end
+})
 
 vim.cmd([[
 set splitbelow
@@ -45,6 +49,7 @@ Plug 'tanvirtin/monokai.nvim'
 Plug 'towolf/vim-helm'
 Plug 'mrjosh/helm-ls'
 Plug 'neovim/nvim-lspconfig'
+Plug 'mfussenegger/nvim-lint'
 Plug 'mfussenegger/nvim-ansible'
 Plug 'itchyny/lightline.vim'
 Plug 'hrsh7th/vim-vsnip'
@@ -54,7 +59,6 @@ Plug 'hrsh7th/cmp-buffer'
 Plug 'hrsh7th/cmp-path'
 Plug 'hrsh7th/cmp-cmdline'
 Plug 'hrsh7th/nvim-cmp'
-Plug 'hashivim/vim-terraform'
 Plug 'farmergreg/vim-lastplace'
 Plug 'tpope/vim-fugitive'
 Plug 'HiPhish/rainbow-delimiters.nvim'
@@ -71,7 +75,7 @@ Plug 'iamcco/markdown-preview.nvim', { 'do': 'cd app && npx --yes yarn install' 
 call plug#end()
 ]])
 
-require('monokai').setup({ 
+require('monokai').setup({
     palette = require('monokai').pro,
     italics = false,
 })
@@ -122,7 +126,7 @@ ibl.setup({
     enabled = true,
     debounce = 100,
     scope = { highlight = highlight },
-    indent = { 
+    indent = {
         char = '┊',
         highlight = highlight,
     },
@@ -134,29 +138,39 @@ ibl.setup({
 hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
 
 vim.api.nvim_command([[setlocal spell spelllang=en_us]])
-vim.api.nvim_set_keymap("", "<F2>", ":set paste! paste?<CR>", {noremap = true})
-vim.api.nvim_set_keymap("i", "<F2>", "<ESC>:set paste! paste?<CR>", {noremap = true})
-vim.api.nvim_set_keymap("", "<F3>", ":set number!<CR>", {noremap = true})
-vim.api.nvim_set_keymap("i", "<F3>", "<ESC>:set number!<CR>", {noremap = true})
-vim.keymap.set({"i", "n"}, "<F4>", "<ESC>:IBLToggle<CR>", {silent = true})
-vim.keymap.set({"i", "n"}, "<F5>", "<ESC>:Neotree toggle<CR>", {silent = true})
-vim.keymap.set({"i", "n"}, "<F7>", "<ESC>:bprevious<CR>", {silent = true})
-vim.keymap.set({"i", "n"}, "<F8>", "<ESC>:bNext<CR>", {silent = true})
-vim.keymap.set({"i", "n"}, "<F12>", ":source $MYVIMRC<CR>", {silent = true})
+vim.api.nvim_set_keymap("", "<F2>", ":set paste! paste?<CR>", { noremap = true })
+vim.api.nvim_set_keymap("i", "<F2>", "<ESC>:set paste! paste?<CR>", { noremap = true })
+vim.api.nvim_set_keymap("", "<F3>", ":set number!<CR>", { noremap = true })
+vim.api.nvim_set_keymap("i", "<F3>", "<ESC>:set number!<CR>", { noremap = true })
+vim.keymap.set({ "i", "n" }, "<F4>", "<ESC>:IBLToggle<CR>", { silent = true })
+vim.keymap.set({ "i", "n" }, "<F5>", "<ESC>:Neotree toggle<CR>", { silent = true })
+vim.keymap.set({ "i", "n" }, "<F7>", "<ESC>:bprevious<CR>", { silent = true })
+vim.keymap.set({ "i", "n" }, "<F8>", "<ESC>:bNext<CR>", { silent = true })
+vim.keymap.set({ "i", "n" }, "<F12>", ":source $MYVIMRC<CR>", { silent = true })
 
 -- 0 is current buffer
-vim.api.nvim_buf_set_keymap(0, "", "<c-q>", "<cmd>call Black()<cr>", {noremap = true, silent = true})
-vim.api.nvim_buf_set_keymap(0, "i", "<c-q>", "<cmd>call Black()<cr>", {noremap = true, silent = true})
+vim.api.nvim_buf_set_keymap(0, "", "<c-q>", "<cmd>call Black()<cr>", { noremap = true, silent = true })
+vim.api.nvim_buf_set_keymap(0, "i", "<c-q>", "<cmd>call Black()<cr>", { noremap = true, silent = true })
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local kubernetes = require("kubernetes")
+
+-- yamllint
+require("lint").linters_by_ft = {
+    yaml = { "yamllint" },
+}
+vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+    callback = function()
+        require("lint").try_lint()
+    end,
+})
 
 kubernetes.setup(
     {
         schema_strict = true,
         schema_generate_always = false,
         patch = true,
-        yamlls_root = function () return "/usr/local/lib/" end,
+        yamlls_root = function() return "/usr/local/lib/" end,
     }
 )
 
@@ -172,35 +186,48 @@ vim.lsp.config("helm-ls", {
 
 vim.lsp.enable("jedi_language_server")
 vim.lsp.enable("ansiblels")
+
+vim.lsp.config("lua_ls", {
+    cmd = { home .. '/repos/oss/lua-language-server/bin/lua-language-server' },
+    capabilities = capabilities,
+})
 vim.lsp.enable("lua_ls")
-vim.lsp.enable("yamlls")
+
+vim.lsp.config("jsonls", {
+    capabilities = capabilities,
+})
+vim.lsp.enable("jsonls")
+
 vim.lsp.config("yamlls", {
     capabilities = capabilities,
     settings = {
         ["yaml"] = {
             schemas = {
                 [kubernetes.yamlls_schema()] = "*.yaml",
-                ["https://repo1.dso.mil/big-bang/bigbang/-/raw/master/chart/values.schema.json?ref_type=heads"] = "/*/configmap.yaml",
-                ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = ".gitlab-ci.yml",
+                ["https://repo1.dso.mil/big-bang/bigbang/-/raw/master/chart/values.schema.json?ref_type=heads"] =
+                "/*/configmap.yaml",
+                ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] =
+                ".gitlab-ci.yml",
             }
         }
     }
 })
+vim.lsp.enable("yamlls")
 
-vim.lsp.enable("terraformls")
 vim.lsp.config("terraformls", {
     capabilities = capabilities
 })
+vim.lsp.enable("terraformls")
 
-vim.lsp.enable("tflint")
 vim.lsp.config("tflint", {
     capabilities = capabilities
 })
+vim.lsp.enable("tflint")
 
-vim.lsp.enable("marksman")
 vim.lsp.config("marksman", {
     capabilities = capabilities
 })
+vim.lsp.enable("marksman")
 
 local cmp = require('cmp')
 
@@ -222,30 +249,29 @@ cmp.setup({
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
     }),
     sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'vsnip' },
-        { name = 'path' },
-    },
-    {
-        { name = 'buffer' },
-    }),
+            { name = 'nvim_lsp' },
+            { name = 'vsnip' },
+            { name = 'path' },
+        },
+        {
+            { name = 'buffer' },
+        }),
 })
 
 vim.diagnostic.config({
-    signs = { 
-        text = { 
+    signs = {
+        text = {
             [vim.diagnostic.severity.ERROR] = '✘',
             [vim.diagnostic.severity.WARN] = '⚠',
-            ... 
+            ...
         }
     }
 })
 
-
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("lsp_attach_auto_diag", { clear = true }),
     callback = function(args)
-        local opts = { noremap = true, silent = true}
+        local opts = { noremap = true, silent = true }
         vim.api.nvim_buf_set_keymap(args.buf, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
         vim.api.nvim_buf_set_keymap(args.buf, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
         vim.api.nvim_buf_set_keymap(args.buf, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
@@ -270,11 +296,13 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end,
 })
 
-require('nvim-treesitter.configs').setup({highlight={enable=true}})  -- At the bottom of your init.vim, keep all configs on one line
-EOF
+require('nvim-treesitter.config').setup({
+    highlight = { enable = true }
+}) -- At the bottom of your init.vim, keep all configs on one line
 
+vim.cmd([[
 let g:lightline = {
-      \ 'colorscheme': 'one', 
+      \ 'colorscheme': 'one',
       \ 'active': {
       \   'left': [ [ 'mode', 'paste' ],
       \             [ 'gitbranch', 'readonly', 'filename', 'modified' ] ]
@@ -311,6 +339,7 @@ augroup yaml_fix
     autocmd FileType yaml,yml setlocal ts=2 sts=2 sw=2 expandtab "indentkeys-=0# indentkeys-=<:>
 augroup END
 
-set list lcs=tab:\»\ 
+set list lcs=tab:\»\
 
 set noshowmode
+]])
